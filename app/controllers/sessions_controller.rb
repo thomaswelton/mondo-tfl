817a814427868e8714d5eea37fb015c0baae922f
James Hill <james.f.hill@gmail.com>
2016-06-07 19:21:17 +0100

    Typo.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
     begin
       @user = User.from_omniauth(request.env['omniauth.auth'])
       session[:user_id] = @user.id
-      redirect_to root_path
     rescue
       flash[:warning] = "There was an error while trying to authenticate you..."
     end
