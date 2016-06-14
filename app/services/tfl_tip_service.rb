80df12966caf7af0ddf394e3eeeaafd4d90e7538
James Hill <james.f.hill@gmail.com>
2016-06-14 12:43:10 +0100

    don’t send tips to users that didn’t make any journeys.

--- app/services/tfl_tip_service.rb
+++ app/services/tfl_tip_service.rb
@@ -8,15 +8,18 @@ class TFLTipService
   def call
     journey_count = user.journeys.last_month.in_peak_hour.count
     message = "You made #{journey_count} peak hour #{'journey'.pluralize(journey_count)} last month."
-
-    puts "sending message to #{user.name}"
-    puts "\t#{message}"
-
-    user.mondo.create_feed_item(
-      title: "Mondo TFL Stats",
-      image_url: "https://raw.githubusercontent.com/jameshill/mondo-tfl/master/app/assets/images/mondo_tfl.png",
-      body: message,
-      url: 'https://mondo-tfl.herokuapp.com/tips/peak',
-    )
+    puts "--- #{user.name} ---"
+    if journey_count == 0
+      puts "\tno message sent, as no journeys."
+      return
+    else
+      puts "\t#{message}"
+      user.mondo.create_feed_item(
+        title: "Mondo TFL Stats",
+        image_url: "https://raw.githubusercontent.com/jameshill/mondo-tfl/master/app/assets/images/mondo_tfl.png",
+        body: message,
+        url: 'https://mondo-tfl.herokuapp.com/tips/peak',
+      )
+    end
   end
 end
